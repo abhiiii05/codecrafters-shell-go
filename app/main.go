e6857386312a07d7d0d13bb75f5c1c7cbcf1ad92
abhiiii.iiii <abhijithviju2005@gmail.com>
2026-09-13 04:28:58 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -20,8 +20,8 @@ func main() {
 		if command == "exit"{
 			break
 		}else if strings.HasPrefix(command, "echo"){
-			result := strings.TrimPrefix(command,"echo")
-			fmt.Print(result)
+			result := strings.TrimPrefix(command,"echo ")
+			fmt.Println(result)
 		}else{
 			fmt.Println(command+": command not found")
 		}
