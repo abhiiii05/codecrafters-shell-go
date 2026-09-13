712e06214615861a011f41586a00cfcc5a0802d5
abhiiii.iiii <abhijithviju2005@gmail.com>
2026-09-13 16:40:46 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -25,8 +25,8 @@ func main() {
 			fmt.Println(result)
 		}else if strings.HasPrefix(command,"type"){
 			prefix := strings.TrimPrefix(command," type")
-			fmt.Println("here")
-			if (prefix == "echo" || prefix =="exit" || prefix=="type"){
+			fmt.Println(prefix)
+			if (prefix == "echo" || prefix == "exit" || prefix== "type"){
 				fmt.Println(prefix + "is a shell builtin")
 			}
 		}else{
